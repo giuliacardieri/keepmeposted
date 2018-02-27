c18e5ed0cf40d14a54666d4917775ab7afb8eb1d
Giulia de Andrade Cardieri <giuliacardieri@users.noreply.github.com>
2018-02-26 22:30:37 -0300

    Update database.php
    Removed private information

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'bNtMb7sV',
-	'database' => 'keepmeposted',
+	'hostname' => 'YOUR-HOST',
+	'username' => 'YOUR-USERNAME',
+	'password' => 'YOUR-PASSWORD',
+	'database' => 'YOUR-DATABASE',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
